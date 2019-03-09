3603c5b7748d6d4c31d7a52bf1e06d197624a6d6
phuong_tran <phuongg.trann8799@gmail.com>
2019-03-09 17:28:48 -0500

    NullPointer found at RegisterAClass

--- Project 2/Registrar.java
+++ Project 2/Registrar.java
@@ -1,3 +1,4 @@
+
 /**
  * The main class in the student registration program.. 
  * This class defines public methods to provide the following functionality:
@@ -10,213 +11,241 @@
 import java.util.Scanner;
 
 public class Registrar {
-  
-/**
- * Attributes.
- */
- private Student [] students;
- private int numStudents; // keeps track of the students array size
- private Course [] courseCatalog;
- private int numCourses;  // keeps track of the courseCatalog array size
- private String semester;
- private int year;
- 
-/**
- * Constructor.
- */
- public Registrar(String semester, int year) {
-  // TODO.
-  // note: maximum number of students is 100, and maximum number of courses is 50.
-  this.semester=semester;
-  this.year=year;
-  if (numStudents>100) {
-    this.students= new Student[100];}
-  else {
-    this.students= new Student[numStudents];}
-  if (numCourses>50) {
-    this.courseCatalog=new Course[50];
-  }
-  else {
-    this.courseCatalog= new Course[numCourses];
-  }
- }
 
-/**
- * Accessor methods.
- */
- public void setSemester(String semester) { this.semester=semester;}
- public String getSemester() { return semester; }
- 
- public void setYear(int year) { this.year=year;}
- public int getYear() { return year; }
+  /**
+   * Attributes.
+   */
+  private Student[] students;
+  private int numStudents; // keeps track of the students array size
+  private Course[] courseCatalog;
+  private int numCourses; // keeps track of the courseCatalog array size
+  private String semester;
+  private int year;
 
- // self-made
- public void setStudents(int numStudents) { this.students = new Student[numStudents];}
- public Student[] getStudents() { return this.students; }
-
- public void setCourseCatalog(int numCourse) { this.courseCatalog = new Course[numCourse];}
-
- 
-/**
- * Public methods.
- */      
- public boolean addCourse(String code, String title, int hours) { 
- // if courseCatalog is full , return false, otherwise, add course to the catalog.
- // TODO
-  if (this.courseCatalog.length>=50) {return false;}
-  else {
-    Course[] temp = new Course[this.courseCatalog.length+1];
-    for (int a=0; a<this.courseCatalog.length;a++) {
-      temp[a]=this.courseCatalog[a];
-    }  
-    temp[temp.length-1]=new Course(code,title,hours);
-    this.courseCatalog=temp;
-    return true;
-  }
-  }
- 
- public String getCourseCatalog() {
-   String str="";
-   for (Course c : courseCatalog)
-     if (c == null) break;
-     else str = str + c.fullString() +"\n";
-   return str;
-   }
- 
- public boolean addStudent(String fname, String lname, long gnum, String major, 
-                           String degree, String highSchool) {  
- // if the students array is full, return false.
- // otherwise add the student information to the students arrays
- // TODO
- //check 
-  if (this.students.length>=100) {
-    return false;
-  }
-  else {
-    Student[] temp = new Student[this.students.length+1];
-    for (int a=0; a<this.students.length; a++) {
-      temp[a]=this.students[a];
+  /**
+   * Constructor.
+   */
+  public Registrar(String semester, int year) {
+    // TODO.
+    // note: maximum number of students is 100, and maximum number of courses is 50.
+    this.semester = semester;
+    this.year = year;
+    this.numStudents=0;
+    this.numCourses=0;
+    if (this.numStudents >= 100) {
+      this.students = new Student[100];
+    } else {
+      this.students = new Student[numStudents];
     }
-    temp[temp.length-1]=new Undergraduate(fname, lname, gnum, major, degree, highSchool);
-    this.students=temp;
-    return true;
+    if (this.numCourses >= 50) {
+      this.courseCatalog = new Course[50];
+    } else {
+      this.courseCatalog = new Course[numCourses];
     }
   }
- 
-  public boolean addStudent(String fname, String lname, long gnum, String major, 
-                            String degree, String uMajor, String uInstit) {  
+
+  /**
+   * Accessor methods.
+   */
+  public void setSemester(String semester) {
+    this.semester = semester;
+  }
+
+  public String getSemester() {
+    return semester;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  // self-made
+  public void setStudents(int numStudents) {
+    this.students = new Student[numStudents];
+  }
+
+  public Student[] getStudents() {
+    return this.students;
+  }
+
+  public void setCourseCatalog(int numCourse) {
+    this.courseCatalog = new Course[numCourse];
+  }
+
+  /**
+   * Public methods.
+   */
+  public boolean addCourse(String code, String title, int hours) {
+    // if courseCatalog is full , return false, otherwise, add course to the
+    // catalog.
+    // TODO
+    if (this.courseCatalog.length >= 50) {
+      return false;
+    } 
+    else {
+      Course[] temp = new Course[this.courseCatalog.length + 1];
+      for (int a = 0; a < this.courseCatalog.length; a++) {
+        temp[a] = this.courseCatalog[a];
+      }
+      temp[temp.length - 1] = new Course(code, title, hours);
+      this.courseCatalog = temp;
+      return true;
+    }
+  }
+
+  public String getCourseCatalog() {
+    String str = "";
+    for (Course c : courseCatalog)
+      if (c == null)
+        break;
+      else
+        str = str + c.fullString() + "\n";
+    return str;
+  }
+
+  public boolean addStudent(String fname, String lname, long gnum, String major, String degree, String highSchool) {
+    // if the students array is full, return false.
+    // otherwise add the student information to the students arrays
+    // TODO
+    // check
+    if (this.students.length >= 100) {
+      return false;
+    } 
+    else {
+      Student[] temp = new Student[this.students.length + 1];
+      for (int a = 0; a < this.students.length; a++) {
+        temp[a] = this.students[a];
+      }
+      temp[temp.length - 1] = new Undergraduate(fname, lname, gnum, major, degree, highSchool);
+      this.students = temp;
+      return true;
+    }
+  }
+
+  public boolean addStudent(String fname, String lname, long gnum, String major, String degree, String uMajor,
+      String uInstit) {
     // an overloaded method which does the sam as the other addStudent method
     // TODO
-    if (this.students.length>=100) {return false;}
+    if (this.students.length >= 100) {
+      return false;
+    } 
     else {
-      Student[] temp = new Student[this.students.length+1];
-      for (int a=0; a<this.students.length; a++) {
-        temp[a]=this.students[a];
+      Student[] temp = new Student[this.students.length + 1];
+      for (int a = 0; a < this.students.length; a++) {
+        temp[a] = this.students[a];
       }
-      temp[temp.length-1]=new Graduate(fname, lname, gnum, major, degree, uMajor, uInstit);
-      this.students=temp;
+      temp[temp.length - 1] = new Graduate(fname, lname, gnum, major, degree, uMajor, uInstit);
+      this.students = temp;
       return true;
-      }
+    }
   }
- 
+
   public boolean register(long gnum, String courseCode) {
-    // first check if the course is offered (by searching courseCatalog array using courseCode), if 
+    // first check if the course is offered (by searching courseCatalog array using
+    // courseCode), if
     // the course is not found return false.
-    // then find the student object in the students array using their gnum, if no student is found 
+    // then find the student object in the students array using their gnum, if no
+    // student is found
     // return false.
     // otherwise register the student in the class and return true.
     // TODO
 
-    for (Course c : this.courseCatalog){
+    for (Course c : this.courseCatalog) {
       if (c.getCode().equals(courseCode)) {
         for (Student s : this.students) {
-          if (s.getGnum()==gnum) {
+          if (s.getGnum() == gnum) {
             if (s.registerAClass(c, this.semester, this.year)) {
               return true;
+            } 
+            else {
+              return false;
             }
-            else {return false;}
-          } 
+          }
         }
       }
     }
     return false;
   }
-        
 
-  
   public boolean drop(long gnum, String courseCode) {
-    // Find the student object in the students array using their gnum, if no student is found 
-    // return false.  otherwise drop the course for the student.
+    // Find the student object in the students array using their gnum, if no student
+    // is found
+    // return false. otherwise drop the course for the student.
     // TODO
     for (Student s : this.students) {
-      if (s.getGnum()==(gnum)) {
-        for (Course c: this.courseCatalog) {
+      if (s.getGnum() == gnum) {
+        for (Course c : this.courseCatalog) {
           if (c.getCode().equals(courseCode)) {
             if (s.dropAClass(courseCode)) {
               return true;
+            } else {
+              return false;
             }
-            else {return false;}
           }
         }
       }
     }
     return false;
   }
-  
+
   public boolean postGrade(long gnum, String courseCode, int score) {
-    // Find the student object in the students array using their gnum, if no student is found 
-    // return false.  otherwise post a course grade for the student.
+    // Find the student object in the students array using their gnum, if no student
+    // is found
+    // return false. otherwise post a course grade for the student.
     // TODO
-    for (Student s: students) {
-      if (s.getGnum()==gnum) {
-        for (TranscriptEntry t: s.getTranscripts()) {
-          if (t.getCode()==courseCode) {
-             if (s.obtainAGrade(t.getCode(), score)) {
+    for (Student s : students) {
+      if (s.getGnum() == gnum) {
+        for (TranscriptEntry t : s.getTranscripts()) {
+          if (t.getCode().equals(courseCode)) {
+            if (s.obtainAGrade(t.getCode(), score)) {
               return true;
-             }
-             else {return false;}
+            } else {
+              return false;
+            }
           }
         }
       }
     }
     return false;
   }
-  
-  
+
   public String getProgress(long gnum, String semester, int year) {
     Student s = findStudent(gnum);
-    if (s == null) 
-        return "Student "+gnum+" not found";
+    if (s == null)
+      return "Student " + gnum + " not found";
     String str = String.format(" %s\n", s.toString());
-    return str+s.getClassList(semester, year);
+    return str + s.getClassList(semester, year);
   }
 
   public static String getDeptName(String code) {
     // given a course code, this static method will return the name
     // of the academic department to which the course belong
     String dept = (new Scanner(code)).next();
-    
-    if (dept.equalsIgnoreCase("CS")) 
-          return "Computer Science";
+
+    if (dept.equalsIgnoreCase("CS"))
+      return "Computer Science";
     else if (dept.equalsIgnoreCase("INFS"))
-          return "Information Systems";
-    else if (dept.equalsIgnoreCase("IT")) 
-          return "Information Tecchnoloy";
-    else if (dept.equalsIgnoreCase("MATH")) 
-          return "Mathematics";
+      return "Information Systems";
+    else if (dept.equalsIgnoreCase("IT"))
+      return "Information Tecchnoloy";
+    else if (dept.equalsIgnoreCase("MATH"))
+      return "Mathematics";
     else if (dept.equalsIgnoreCase("PHYS"))
-          return "Physics";            
-    else return null;
+      return "Physics";
+    else
+      return null;
   }
-  
-  
-/**
- * Private methods.
- * you will need few helper methods.
- */ 
-  public Student findStudent(long gnum){
-    for (Student a: this.students) {
-      if (a.getGnum()==gnum) {
+
+  /**
+   * Private methods. you will need few helper methods.
+   */
+  public Student findStudent(long gnum) {
+    for (Student a : this.students) {
+      if (a.getGnum() == gnum) {
         return a;
       }
     }
--- Project 2/Student.java
+++ Project 2/Student.java
@@ -1,246 +1,245 @@
 
 /**
- * An abstract class for maintaing basic information about a student attending this college. 
- * This class defines public methods to provide the following functionality to a student object:
- *     - register and drop courses 
- *     - record a grade for a completed course
- *     - get transcripts / a list of completed courses
- * sub-classes of this class will specify how student rgistration is to be performed (depending 
- * on the level of the student), and also grade assignments.
- *     
+ * An abstract class for maintaing basic information about a student attending
+ * this college. This class defines public methods to provide the following
+ * functionality to a student object: - register and drop courses - record a
+ * grade for a completed course - get transcripts / a list of completed courses
+ * sub-classes of this class will specify how student rgistration is to be
+ * performed (depending on the level of the student), and also grade
+ * assignments.
+ * 
  * @author CS211 GMU
  */
 
 public abstract class Student {
-/**
- * Attributes.
- * note how the class is defining its instance variables as protected so 
- * that they are accessible to its sub-classes.
- */
- protected String first, last;
- protected long gnum;
- protected String major;
- protected String degree;
- protected TranscriptEntry [] transcripts; // an array of objects
- protected final int PROGRAM; // a constant which will be initilized to 0 or 1 
-                              // in the constructor method
+  /**
+   * Attributes. note how the class is defining its instance variables as
+   * protected so that they are accessible to its sub-classes.
+   */
+  protected String first, last;
+  protected long gnum;
+  protected String major;
+  protected String degree;
+  protected TranscriptEntry[] transcripts; // an array of objects
+  protected final int PROGRAM; // a constant which will be initilized to 0 or 1
+                               // in the constructor method
 
-/**
- * Attributes.
- * note how the class is defining its instance variables as protected so 
- * that they are accessible to its sub-classes.
- */
- public Student(int level, String first, String last, long gnum, String major, String degree) {
-  // TODO initialize the instance variables
-  //  Also use the parameter variable "level" as follows
-  //     1) use its value to initialized the PROGRAM constant
-  //     2) create the transcripts array to be of size [50] if is level 0, 
-  //        or to be of size [15] if level is 1.  
-  // note that level refers to the student type and we use a value of 0 for 
-  // an undergrad and a value of 1 for a grad
-  this.first=first;
-  this.last=last;
-  this.gnum=gnum;
-  this.major=major;
-  this.degree=degree;
-  this.PROGRAM=level;
-  if (level==0) {
-    this.transcripts=new TranscriptEntry[50];} // Undergrad
-  if (level==1) {
-    this.transcripts=new TranscriptEntry[15];} // Grad
- }
+  /**
+   * Attributes. note how the class is defining its instance variables as
+   * protected so that they are accessible to its sub-classes.
+   */
+  public Student(int level, String first, String last, long gnum, String major, String degree) {
+    // TODO initialize the instance variables
+    // Also use the parameter variable "level" as follows
+    // 1) use its value to initialized the PROGRAM constant
+    // 2) create the transcripts array to be of size [50] if is level 0,
+    // or to be of size [15] if level is 1.
+    // note that level refers to the student type and we use a value of 0 for
+    // an undergrad and a value of 1 for a grad
+    this.first = first;
+    this.last = last;
+    this.gnum = gnum;
+    this.major = major;
+    this.degree = degree;
+    this.PROGRAM = level;
+    if (level == 0) {
+      this.transcripts = new TranscriptEntry[50];
+    } // Undergrad
+    if (level == 1) {
+      this.transcripts = new TranscriptEntry[15];
+    } // Grad
+  }
 
- /**
- * Accessor methods.
- */
-// TODO: define a setter and a getter method for each of the instance variables
-public String getFirst() {return this.first;}
-public void setFirst(String first) {this.first=first;}
+  /**
+   * Accessor methods.
+   */
+  // TODO: define a setter and a getter method for each of the instance variables
+  public String getFirst() {
+    return this.first;
+  }
 
-public String getLast() {return this.last;}
-public void setLast(String last) {this.last=last;}
+  public void setFirst(String first) {
+    this.first = first;
+  }
 
-public int getProgram() {return this.PROGRAM;}
-// final instance doesn't have setter
+  public String getLast() {
+    return this.last;
+  }
 
-public long getGnum() {return this.gnum;}
-public void setGnum(long gnum) {this.gnum=gnum;}
+  public void setLast(String last) {
+    this.last = last;
+  }
 
-public String getMajor() {return this.major;}
-public void setMajor(String major) {this.major=major;}
+  public int getProgram() {
+    return this.PROGRAM;
+  }
+  // final instance doesn't have setter
 
-public String getDegree() {return this.degree;}
-public void setDegree(String degree) {this.degree=degree;}
+  public long getGnum() {
+    return this.gnum;
+  }
 
-public TranscriptEntry[] getTranscripts() {return this.transcripts;}
-public void setTranscipts(int level) {
-  if (level==0) {this.transcripts=new TranscriptEntry[50];}
-  else {this.transcripts=new TranscriptEntry[15];}
-}
+  public void setGnum(long gnum) {
+    this.gnum = gnum;
+  }
 
- /**
- * Public methods.
- */
-  public boolean registerAClass(Course c, String semester, int year) { 
-   // this method will call the approvedForClass method defined by 
-   // the subclasses before enrolling the student into the course.
-   //  This method also checks if the student is already enrolled in 
-   // in the current semester and will return false if thats the case.
-   // hint: a student would be currently enrolled if the .isActive() method returned true.
-//     if (approvedForClass(c)) {
-//       for (TranscriptEntry t: this.transcripts) {
-//         if (t.equals(c)) {
-//           if (t.isActive()) {return false;} // is doing that course rn
-//           else {return false;}  // enrolled in the same course
-//         }
-//       }
-//       TranscriptEntry[] temp = new TranscriptEntry[this.transcripts.length+1];
-//       for (int i=0; i<this.transcripts.length; i++) {
-//         temp[i] = this.transcripts[i];
-//       }
-//       // Register course and return true
-//       temp[temp.length-1] = new TranscriptEntry(c, semester, year);
-//       this.transcripts=temp;
-//       return true; 
-//     }
-//     else {return false;} // course not approved
-//  }
-boolean canAdd= true;
-  if (this.approvedForClass(c)) {
-    for (int i = 0; i < this.transcripts.length; i++) {
-      if (this.transcripts[i].equals(c) && this.transcripts[i].isActive()) {
-          canAdd=false;
-      }
-      else{
-        TranscriptEntry courseToAdd= new TranscriptEntry(c, semester, year);
-        if (this.transcripts[this.transcripts.length-1]!=null){
-          canAdd=false;
-        }
-        else {
-          this.transcripts[this.transcripts.length-1]=courseToAdd;
-          canAdd=true;
-        }
-      }
+  public String getMajor() {
+    return this.major;
+  }
+
+  public void setMajor(String major) {
+    this.major = major;
+  }
+
+  public String getDegree() {
+    return this.degree;
+  }
+
+  public void setDegree(String degree) {
+    this.degree = degree;
+  }
+
+  public TranscriptEntry[] getTranscripts() {
+    return this.transcripts;
+  }
+
+  public void setTranscipts(int level) {
+    if (level == 0) {
+      this.transcripts = new TranscriptEntry[50];
+    } else {
+      this.transcripts = new TranscriptEntry[15];
     }
   }
-  else {canAdd=false;}
-  return canAdd;
-}
-   
-   
-  public boolean dropAClass(String courseCode) {
-  // We may only drop a student if he/she is currently enrolled (ie no grade posted yet).
-  // Find the course entry in transcripts array and literally remove it, don't just
-  // replace it with a null value, shift array elementsleft-ward to replace it!
-  // hint: create a new array when removing a course from the transcripts array
-  //      TODO
 
-  /*  boolean canDrop=false;
-    TranscriptEntry[] temp = new TranscriptEntry[this.transcripts.length];
-    for (int a=0; a<this.transcripts.length; a++) {
-      if ((this.transcripts[a].getCode()).equals(courseCode)) {
-        if (!(this.transcripts[a].getGrade().equals(""))) { // If grade is posted
-          canDrop=false;
-          break;
-        }
-        else {
-          canDrop=true;
-          break;
-        }
-      }
-    }
-    Creating new array to shift element
-    int c=0;
-    for (int b=0; b<temp.length; b++) {
-      while (c<temp.length) {
-        if (this.transcripts[b].getCode().equals(courseCode)) {
-          continue;
-        }
-        else {
-          temp[c]=this.transcripts[b];
-          c++;
-        }
-      }
-    }
-    this.transcripts=temp;
-    return canDrop; // if course is not in transcript
-  }
-*/
-  boolean needToReplace = false;
-  int deletedIndex = 0;
-  for (int i = 0; i < this.transcripts.length; i++) {
-    if (this.transcripts[i].getCode().equals(courseCode) && this.transcripts[i].isActive()) {
-      this.transcripts[i] = null;
-      deletedIndex = i;
-      needToReplace = true;
-    }
-  }
-  if (needToReplace) {
-    for (int i = deletedIndex; i < this.transcripts.length; i++) {
-      this.transcripts[i] = this.transcripts[i + 1];
-    }
-  }
-  return needToReplace;
-}
-
- public boolean obtainAGrade(String courseCode, int score) {     
-   // this method will search the transcripts array to find a course and will
-   // then assigne a letter grade for the student in that course.  if the course is not found 
-   // in the array, or if its a past course then return false (should overwrite a past course grade).
-      //TODO
+  /**
+   * Public methods.
+   */
+  public boolean registerAClass(Course c, String semester, int year) {
+    // this method will call the approvedForClass method defined by
+    // the subclasses before enrolling the student into the course.
+    // This method also checks if the student is already enrolled in
+    // in the current semester and will return false if thats the case.
+    // hint: a student would be currently enrolled if the .isActive() method
+    // returned true.
+    boolean canAdd=false;
+    if (approvedForClass(c)) {
       for (TranscriptEntry t: this.transcripts) {
-        if (t.getCode().equals(courseCode)) {
+        if (t.equals(c)) {
           if (t.isActive()) {
-            if (t.getGrade().equals("")) {
-              setCourseGrade(t, score);
-              return true;
-            }
-            else {
-              setCourseGrade(t, score);
-              return false;
-            }
-          }
+            canAdd=false;
+            break; } // is doing that course rn
+          else {
+            canAdd=true;
+            break; }
         }
       }
-      return false;
- }
+    }
+    if (canAdd) {
+      TranscriptEntry[] temp = new TranscriptEntry[this.transcripts.length+1];
+      for (int i=0; i<this.transcripts.length; i++) {
+        temp[i] = this.transcripts[i];
+      }
+      // Register course and return true
+      temp[temp.length-1] = new TranscriptEntry(c, semester, year);
+      this.transcripts=temp;
+    }
+    else {canAdd= false;} // course not approved
+  return canAdd;
+  }
+
+  public boolean dropAClass(String courseCode) {
+    // We may only drop a student if he/she is currently enrolled (ie no grade
+    // posted yet).
+    // Find the course entry in transcripts array and literally remove it, don't
+    // just replace it with a null value, shift array elementsleft-ward to replace it!
+    // hint: create a new array when removing a course from the transcripts array
+    // TODO
+    boolean canDrop=false;
+    TranscriptEntry classDrop=null;
+    for (TranscriptEntry t: this.transcripts) { 
+      if ((t.getCode()).equals(courseCode)) { 
+        if (t.getGrade().equals("")) { // If grade isn't posted
+          canDrop = true; 
+          classDrop=t;
+          break;
+        }
+        else {canDrop=false; break;}
+      }
+    }
+    if (canDrop) {
+      TranscriptEntry[] temp = new TranscriptEntry[this.transcripts.length]; 
+      int a = 0;
+      for (int x=0; x<this.transcripts.length; x++) {
+        if (this.transcripts[x].equals(classDrop)) {continue;}
+        else {
+          temp[a]=this.transcripts[x];
+          a++;
+        }
+      }
+      this.transcripts=temp;
+    }
+      return canDrop; // if course is not in transcript 
+  }
+
+  public boolean obtainAGrade(String courseCode, int score) {
+    // this method will search the transcripts array to find a course and will
+    // then assigne a letter grade for the student in that course. if the course is
+    // not found
+    // in the array, or if its a past course then return false (should overwrite a
+    // past course grade).
+    // TODO
+    for (TranscriptEntry t : this.transcripts) {
+      if (t.getCode().equals(courseCode)) {
+        if (t.isActive()) {
+          setCourseGrade(t, score);
+          return true;
+        } else {
+          setCourseGrade(t, score);
+          return false;
+        }
+      }
+    }
+    return false;
+  }
 
   public String getClassList(String semester, int year) {
-    String str=String.format(" %s %d\n", semester, year);
+    String str = String.format(" %s %d\n", semester, year);
     for (TranscriptEntry en : transcripts) {
-            if (en == null) break;
-      else if ((en.getSemester().equalsIgnoreCase(semester)) && 
-               (en.getYear()==year))
-         str += en.toString() + "\n";
-    }  
+      if (en == null)
+        break;
+      else if ((en.getSemester().equalsIgnoreCase(semester)) && (en.getYear() == year))
+        str += en.toString() + "\n";
+    }
     return str;
-  }  
+  }
 
- @Override 
- public boolean equals(Object o) {
-   // two student onjects are equal if they have the same G#
-   // TODO
-   if (o instanceof Student) {
-     Student a = (Student) o;
-     if (this.gnum==a.getGnum()) {return true;}
-   }
-   return false;
- }
- 
- @Override
- public String toString() {
-   // return a string with the following format:
-   //  �Smith, John (G#0000000000)� 
-   //TODO
-   return String.format("%s, %s (G#%s", this.last,this.first,this.gnum);
- }
+  @Override
+  public boolean equals(Object o) {
+    // two student onjects are equal if they have the same G#
+    // TODO
+    if (o instanceof Student) {
+      Student a = (Student) o;
+      if (this.gnum == a.getGnum()) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-/**
- * Private methods.
- * you may add as much helper methods as you need. 
- * 
- */
- 
-  protected abstract boolean approvedForClass(Course c); 
+  @Override
+  public String toString() {
+    // return a string with the following format:
+    // �Smith, John (G#0000000000)�
+    // TODO
+    return String.format("%s, %s (G#%s", this.last, this.first, this.gnum);
+  }
+
+  /**
+   * Private methods. you may add as much helper methods as you need.
+   * 
+   */
+
+  protected abstract boolean approvedForClass(Course c);
+
   protected abstract void setCourseGrade(TranscriptEntry entry, int score);
 }
