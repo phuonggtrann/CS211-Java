41edc0d98c183d5533c79d8be733e76a315e57fe
phuong_tran <phuongg.trann8799@gmail.com>
2019-03-13 20:52:29 -0400

    Add comments for TranscriptEntry.java

--- Project 2/TranscriptEntry.java
+++ Project 2/TranscriptEntry.java
@@ -4,10 +4,7 @@
  * @author CS211 GMU
  */
 public class TranscriptEntry extends Course {
-/**
- * Attributes
- * 
- */
+// Declare attributes
   private String semester;
   private int year;
   private String grade;
@@ -17,12 +14,13 @@ public class TranscriptEntry extends Course {
   public TranscriptEntry(Course c, String semester, int year) {
     // TODO
     // note: active must be initialized to true.
-    super(c.getCode(),c.getTitle(),c.getCredits());
+    // Initialize attributes
+    super(c.getCode(),c.getTitle(),c.getCredits()); // since this is a child class, we call super() to acces parents' function
     this.c=c;
     this.semester=semester;
     this.year=year;
     this.grade="";
-    this.active=true;
+    this.active=true; // active must be true
 
 
   }     
@@ -35,6 +33,8 @@ public class TranscriptEntry extends Course {
  *       if a course is flagged active, then the student is currently enrolled in that course.
  *       When a grade is posted for a course, active is set to false.
  */
+
+ // Setter and getter method for attributes
 public Course getC() {return this.c;}
 public void setC(Course c) {this.c=c;}
 
@@ -52,26 +52,29 @@ public void setGrade(String grade) {this.grade=grade;}
    * 
    */
   
-  public boolean isActive() {
-   //TODO
-   if (getGrade().equals("")) {
-     this.active=true;
-   }
-   else {
-     this.active=false;
-    }
-   return this.active;
+public boolean isActive() {
+  //TODO
+  if (getGrade().equals("")) { // if no grade is posted then isActive() is true
+    this.active=true;
+  }
+  else { // if grade has posted then isActive become false
+    this.active=false;
+  }
+  return this.active;
   }
  
-  
  @Override
  public String toString() { 
    // return a string with the following format:
    //  "\tINFS 510 Database Systems,   credits: 3, GRADE: A" (for students have assigned grade)
    //  "\tINFS 510 Database Systems,   credits: 3" (for student who doesnt have an assigned grade)
    //TODO
-   if (this.grade.equals("")) {return String.format("\t%s %s,   credits: %d", super.getCode(),super.getTitle(),super.getCredits());}
+   // If no grade is posted, toString has different format therefore if-else is used
+   if (this.grade.equals("")) { 
+     return String.format("\t%s %s,   credits: %d", super.getCode(),super.getTitle(),super.getCredits());
+    }
    else {
-   return String.format("\t%s %s,   credits: %d, GRADE: %s", super.getCode(),super.getTitle(),super.getCredits(),this.getGrade()); }
+     return String.format("\t%s %s,   credits: %d, GRADE: %s", super.getCode(),super.getTitle(),super.getCredits(),this.getGrade()); 
+    }
  }
 }
