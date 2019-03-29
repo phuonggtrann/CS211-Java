39773ff8d3c567bf71e0256855774604620e30a4
phuong_tran <phuongg.trann8799@gmail.com>
2019-03-29 13:52:27 -0400

    Add comments

--- Lab 10/TopResult.java
+++ Lab 10/TopResult.java
@@ -1,28 +1,32 @@
-public class TopResult< T extends Comparable<T> > {
-    private T top;
-    public TopResult() {
-        this.top=null;
+public class TopResult<T extends Comparable<T>> { // Declare generic class, extend compareble class 
+    private T top; // declare variable
+
+    public TopResult() { // constructor to initialize variable
+        this.top = null; 
     }
-    public void newResult(T a) {
-        if (this.top==null) {this.top=a;}
-        else{
-            if (a.compareTo(this.top)>0) {
-                this.top=a;
+
+    public void newResult(T a) { // compare parameter object to "this.top" object
+        if (this.top == null) { // if there is no this.top yet, assign that value to this.top
+            this.top = a;
+        } else {
+            if (a.compareTo(this.top) > 0) { // else compare and if method return a positive, assign new this.top
+                this.top = a;
             }
         }
     }
-    public T getTopResult() {
+
+    public T getTopResult() { // getter
         if (this.top == null) {
             return null;
         } else {
             return this.top;
         }
     }
-    public String toString() {
+
+    public String toString() { // calling toString method
         if (this.top == null) {
             return null;
-        }
-        else {
+        } else {
             return this.top.toString();
         }
     }
