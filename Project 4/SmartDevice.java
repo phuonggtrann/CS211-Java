94505cc5396f5ff5ff96ba5898d64fa75ef933e5
phuong_tran <phuongg.trann8799@gmail.com>
2019-04-04 23:47:58 -0400

    implement SmartDevice.java

---
+++ Project 4/SmartDevice.java
@@ -0,0 +1,19 @@
+public class SmartDevice implements DeviceInfo{
+
+    private String name;
+    private int id;
+
+    // Constructor
+    public SmartDevice(String name) {
+        this.name=name;
+        this.id=0;
+    }
+    public int id() {return this.id;}
+    public void setID(int id) {this.id=id;}
+
+    public String name() {return this.name;}
+    public String toString() {
+        return this.name+id();
+    }
+
+}
