05a6a23260ab1317b07cc4a4a96bfcdc5155a3d7
phuong_tran <phuongg.trann8799@gmail.com>
2019-04-25 23:13:08 -0400

    Finish implementing PhoneBookUtils.java

--- Project 5/P5tester.java
+++ Project 5/P5tester.java
@@ -10,23 +10,68 @@ public class P5tester {
   public static void main(String[] args) {
     org.junit.runner.JUnitCore.main("P5tester");
   }
+  // Test Contact task
   @Test 
-  public void testContact_toString() {
+  public void testToString() {
     Contact c = new Contact("Arthur Moddy", "est@temp.com", "703­555­5555");
     assertEquals("Arthur Moddy, email: est@temp.com, phone: 703­555­5555.",c.toString());
   }
-  
+  //Test phone book utils task
   @Test
-  public void testPhoneBookUtils_mapToString(){
+  public void testMapToString(){
     Map<String, Integer> map = new HashMap<String, Integer>();
     map.put("x",1); map.put("y",2); map.put("z",3);
     assertEquals("1\n2\n3\n", PhoneBookUtils.mapToString(map));
   }
 
+  Integer[] ints = { 3, 8, 5, 4, 1, 9, -2};
+  List<Integer> intList = new ArrayList<Integer>(Arrays.asList(ints));
+  String[] strs = { "Hhh", "Ccc", "Fff", "Ttt", "Aaa", "Ddd" };
+  List<String> strList = new ArrayList<String>(Arrays.asList(strs));
+
   @Test 
-  public void testPhoneBookUtils_listToSortedList(){
-    Integer[] ints = { 3, 8, 5, 4, 1, 9, -2};
-    List<Integer> intList = new ArrayList<Integer>(Arrays.asList(ints));
+  public void testListToSortedList0(){
     assertEquals("-2\n1\n3\n4\n5\n8\n9\n", PhoneBookUtils.listToSortedList(intList));
   }
+
+@Test 
+  public void testListToSortedList1(){
+    assertEquals("Aaa\nCcc\nDdd\nFff\nHhh\nTtt\n", PhoneBookUtils.listToSortedList(strList));
+  }
+
+  // @Test 
+  // public void testInsertionSort0(){
+  //   assertEquals("[-2, 1, 3, 4, 5, 8, 9]", java.util.Arrays.toString(ints));
+  // }
+
+  @Test 
+  public void testPhoneBookUtils_binarySearch0(){
+    Integer expected = 9;
+    assertEquals(expected, PhoneBookUtils.binarySearch(ints, 0, ints.length, 9));
+  }
+
+  @Test 
+  public void testPhoneBookUtils_binarySearch1(){
+    Integer expected = null;
+    assertEquals(expected, PhoneBookUtils.binarySearch(ints, 0, ints.length, 45));
+  }
+
+  @Test 
+  public void testPhoneBookUtils_binarySearch2(){ //Test out of range end point
+    Integer expected = 9;
+    assertEquals(expected, PhoneBookUtils.binarySearch(ints, 0, 20, 9));
+  }
+
+  @Test 
+  public void testPhoneBookUtils_binarySearch3(){ 
+    String expected = "Fff";
+    assertEquals(expected, PhoneBookUtils.binarySearch(strs, 0, strs.length, "Fff"));
+  }
+
+  @Test 
+  public void testPhoneBookUtils_binarySearch4(){ 
+    String expected = null;
+    assertEquals(expected, PhoneBookUtils.binarySearch(strs, 0, strs.length, "Bebe"));
+  }
+
 }
--- Project 5/PhoneBookUtils.java
+++ Project 5/PhoneBookUtils.java
@@ -1,37 +1,49 @@
 import java.util.*;
 
 public final class PhoneBookUtils {
-  public static <KeyType, ValueType> String mapToString(Map <KeyType, ValueType> map){
-      String s = "";
-      //Collection c = map.values();
-      for (ValueType col: map.values()) {
-          s=s+col.toString()+"\n";
-      }
-      return s;
-  } 
-  public static <T extends Comparable<T>> String listToSortedList(List <T> list) {
-      String s ="";
-      Collections.sort(list);
-      for (T t: list) {
-          s=s+t.toString()+"\n";
-      }
-      return s;
-  }
-  public static <T extends Comparable<T>> void insertionSort(T[] arr, int i) {
+    public static <KeyType, ValueType> String mapToString(Map<KeyType, ValueType> map) {
+        String s = "";
+        // Collection c = map.values();
+        for (ValueType col : map.values()) {
+            s = s + col.toString() + "\n";
+        }
+        return s;
+    }
 
-      if (i==arr.length-1) {
-          return;
-      }
-      insertionSort(arr, i+1);
-      int index = i;
-      T temp = arr[index];
-      while (index>0 && arr[index].compareTo(arr[index-1])>0) {
-          if (arr[index].compareTo(arr[i])>0) {
-              index--;
-          }
-          arr[index]=arr[index-1];
-          arr[index-1]=temp;
-      }
+    public static <T extends Comparable<T>> String listToSortedList(List<T> list) {
+        String s = "";
+        Collections.sort(list);
+        for (T t : list) {
+            s = s + t.toString() + "\n";
+        }
+        return s;
+    }
 
-  }
+    public static <T extends Comparable<T>> void insertionSort(T[] arr, int i) {
+
+        if (i >= arr.length - 1) {
+            return;
+        }
+        int index = i + 1;
+        T temp = arr[index];
+        while (index > 0 && arr[index].compareTo(arr[index - 1]) < 0) {
+            arr[index] = arr[index - 1];
+            arr[index - 1] = temp;
+            index--;
+        }
+        insertionSort(arr, i + 1);
+
+    }
+
+    public static <T extends Comparable<T>> T binarySearch(T arr[], int begin, int end, T key) {
+        T t = null;
+        while (begin < end || begin < arr.length) {
+            if (arr[begin].compareTo(key) == 0) {
+                t = arr[begin];
+                break;
+            }
+            begin++;
+        }
+        return t;
+    }
 }
