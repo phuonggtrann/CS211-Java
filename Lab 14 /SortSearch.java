95bbbb0dba2af89226f7d88462f5e29a870bfdb9
phuong_tran <phuongg.trann8799@gmail.com>
2019-04-29 23:36:51 -0400

    Adding comment

--- Lab 14 /SortSearch.java
+++ Lab 14 /SortSearch.java
@@ -11,7 +11,7 @@ public class SortSearch {
     }
 
     public static void sortDist(int[] array, int v) {
-        insertionSort(array, 0, v);
+        insertionSort(array, 0, v); // calling helper method with initial index at 0
 
     }
 
@@ -21,21 +21,22 @@ public class SortSearch {
         // initialize needed variable
         int temp = array[i+1]; 
         int index = i+1;
-        while(index>0){
-            if (Math.abs((array[index]-v)) < Math.abs((array[index-1]-v))) {
-                array[index]=array[index-1];
+        while(index>0){ // while loop to rearrrange index
+            if (Math.abs((array[index]-v)) < Math.abs((array[index-1]-v))) { // Math.abs to find the real distance
+                // if it's closer then rearrange
+                array[index]=array[index-1]; 
                 array[index-1]=temp;
                 
             }
-            index--;
+            index--; // decrement index
         }
-        insertionSort(array, i+1, v);
+        insertionSort(array, i+1, v); // recursive call, increment index by 1
           
     }
 
     public static int findKthFromV(int[] array, int k, int v) {
-        int arr[] = array;
-        sortDist(arr, v);
-        return arr[k - 1];
+        int arr[] = array; // create array copy
+        sortDist(arr, v); // sorted array by invoking helper method
+        return arr[k - 1]; // return array's k-1 index
     }
 }
